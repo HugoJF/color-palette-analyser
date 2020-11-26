b06abed673f041be1b378aae7ff6ad32247e4d7f
Hugo Jeller <hugo_jeller@hotmail.com>
2020-11-26 01:23:45 -0400

    Added tone graphs. Updated design to 3 columns

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import {useSelector} from "react-redux";
 
 export default function App() {
     const [selectedColor, setSelectedColor] = useState('blue');
+    const [selectedTone, setSelectedTone] = useState(500);
+
     const colors = useSelector(state => state.colors);
 
     const tones = Object.keys(Object.values(colors)[0] || {});
+    const names = Object.keys(colors);
 
-    const selected = Object
+    const selected1 = Object
         .entries(colors[selectedColor])
         .map(([id, color]) => ({
             id,
@@ -17,41 +20,53 @@ export default function App() {
             color,
         }));
 
-    return (
-        <div className="py-16 mx-auto container">
-            <div className="grid mx-auto mb-8 w-1/2 container">
-                <div className="grid" style={{gridTemplateColumns: `repeat(${tones.length + 1},minmax(0,1fr))`}}>
-                    {/* Tone colors */}
-                    <div/>
-                    {tones.map(tone => <div className="text-center text-gray-600 font-medium">
-                        {tone}
-                    </div>)}
+    const selected2 = names.map(name => {
+        return {
+            id: selectedTone,
+            name,
+            color: colors[name][selectedTone]
+        };
+    });
 
-                    {/* Color palette */}
-                    {Object.entries(colors).map(([main, p]) => (<>
-                        {/* Color name */}
-                        <div className="mr-3 flex justify-end items-center text-gray-600 font-medium capitalize">
-                            {main}
+    return (
+        <div className="py-16">
+            <div className="px-4 grid grid-cols-3 gap-8">
+                <div className="">
+                    <div className="grid mx-auto mb-8 container">
+                        <div className="grid" style={{gridTemplateColumns: `repeat(${tones.length + 1},minmax(0,1fr))`}}>
+                            {/* Tone colors */}
+                            <div/>
+                            {tones.map(tone => <div className="text-center text-gray-600 font-medium">
+                                {tone}
+                            </div>)}
+
+                            {/* Color palette */}
+                            {Object.entries(colors).map(([main, p]) => (<>
+                                {/* Color name */}
+                                <div className="mr-3 flex justify-end items-center text-gray-600 font-medium capitalize">
+                                    {main}
+                                </div>
+
+                                {/* Color blocks */}
+                                {Object.entries(p).map(([id, c]) => (
+                                    <div
+                                        onClick={() => {
+                                            setSelectedColor(main)
+                                            setSelectedTone(id);
+                                        }}
+                                        style={{backgroundColor: `#${cc.rgb.hex(c)}`}}
+                                        className={`flex-grow h-12`}
+                                    />
+                                ))}
+                            </>))}
                         </div>
 
-                        {/* Color blocks */}
-                        {Object.entries(p).map(([id, c]) => (
-                            <div
-                                onClick={() => setSelectedColor(main)}
-                                style={{backgroundColor: `#${cc.rgb.hex(c)}`}}
-                                className={`flex-grow h-12`}
-                            />
-                        ))}
-                    </>))}
+                    </div>
                 </div>
-
-            </div>
-
-            <div className="flex justify-center">
-                <div className="w-1/2 flex flex-col">
+                <div className="flex flex-col">
                     <Chart
                         title="Hue"
-                        colors={selected}
+                        colors={selected1}
                         colorUpdate={(c, f) => [360 * f, c[1], c[2]]}
                         colorComponentValue={(c) => (cc.rgb.hsl(c)[0])}
                         colorMaxComponentValue={360}
@@ -59,7 +74,7 @@ export default function App() {
 
                     <Chart
                         title="Saturation"
-                        colors={selected}
+                        colors={selected1}
                         colorUpdate={(color, factor) => [color[0], 100 * factor, color[2]]}
                         colorComponentValue={(color) => (cc.rgb.hsl(color)[1])}
                         colorMaxComponentValue={100}
@@ -67,7 +82,32 @@ export default function App() {
 
                     <Chart
                         title="Lightness"
-                        colors={selected}
+                        colors={selected1}
+                        colorUpdate={(color, factor) => [color[0], color[1], 100 * factor]}
+                        colorComponentValue={(color) => (cc.rgb.hsl(color)[2])}
+                        colorMaxComponentValue={100}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <Chart
+                        title="Hue"
+                        colors={selected2}
+                        colorUpdate={(c, f) => [360 * f, c[1], c[2]]}
+                        colorComponentValue={(c) => (cc.rgb.hsl(c)[0])}
+                        colorMaxComponentValue={360}
+                    />
+
+                    <Chart
+                        title="Saturation"
+                        colors={selected2}
+                        colorUpdate={(color, factor) => [color[0], 100 * factor, color[2]]}
+                        colorComponentValue={(color) => (cc.rgb.hsl(color)[1])}
+                        colorMaxComponentValue={100}
+                    />
+
+                    <Chart
+                        title="Lightness"
+                        colors={selected2}
                         colorUpdate={(color, factor) => [color[0], color[1], 100 * factor]}
                         colorComponentValue={(color) => (cc.rgb.hsl(color)[2])}
                         colorMaxComponentValue={100}
